9ba66f15cf79f54fa8748eac33728201cabba20e
wkoju84 <wkoju84@gmail.com>
2023-12-26 15:03:54 -0300

    initial commit

---
+++ src/test/java/br/com/emprescar/EmprescarApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.emprescar;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EmprescarApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
